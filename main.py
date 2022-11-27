c5220c28460cb1b1dd44e5141efdfc6bda0902b9
Nir <nircaf@gmail.com>
2022-11-27 10:04:22 +0200

    me

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import yaml
 import time
-from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support.ui import WebDriverWait
 import random
 from selenium.webdriver.common.by import By
 
@@ -10,7 +10,7 @@ def main():
         cfg = yaml.load(f, Loader=yaml.FullLoader)
 
     option = webdriver.ChromeOptions()
-    # I use the following options as my machine is a window subsystem linux. 
+    # I use the following options as my machine is a window subsystem linux.
     # I recommend to use the headless option at least, out of the 3
     option.add_argument('--headless')
     option.add_argument('--no-sandbox')
@@ -18,9 +18,9 @@ def main():
     option.add_argument('--disable-notifications')
     # Replace YOUR-PATH-TO-CHROMEDRIVER with your chromedriver location
     driver = webdriver.Chrome(executable_path = r'/home/tzag/Nir/me/chromedriver', options=option)
-    
+
     driver.get('https://app.meckano.co.il/login.php#login') # Getting page HTML through request
-    wait=WebDriverWait(driver,30)                                 
+    wait=WebDriverWait(driver,30)
     driver.maximize_window()
     driver.find_element(By.ID, "email").send_keys(cfg['email'])
     driver.find_element(By.ID, "password").send_keys(cfg['p'])
